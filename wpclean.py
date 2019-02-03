85361728d8dfed3867af95fb8f77c9a6b66ab8dc
mriffey@rescuemarketing.com <$Pine1068$$z>
2019-02-03 11:19:23 -0700

    added Beacon sig.

--- wpclean.py
+++ wpclean.py
@@ -7,5 +7,12 @@ txttoclean = txttoclean.replace('<!-- wp:paragraph -->','')
 txttoclean = txttoclean.replace('<!-- /wp:heading -->','')
 txttoclean = txttoclean.replace('<!-- wp:heading -->','')
 
+txttoclean = txttoclean + '<i>Want to learn more about Mark or ask him to write about a strategic, operations or ' +\
+             'marketing problem? See <a class="text" href="http://www.markriffey.com/" target="_blank" ' + \
+             'rel="noopener noreferrer">Mark\'s site</a>, contact him on <a class="text" href="https://' + \
+             'linkedin.com/in/markriffey" target="_blank" rel="noopener noreferrer">LinkedIn</a> or <a class="text" ' + \
+             'href="http://www.twitter.com/MarkRiffey" target="_blank" rel="noopener noreferrer">Twitter</a>, or ' + \
+             'email him at <a class="text" href="mailto:mriffey@flatheadbeacon.com">mriffey@flatheadbeacon.com</a>.</i>'
+
 pyperclip.copy(txttoclean)
 print(txttoclean)
