6a968bd8d004115e9fe408eec1aa7a9ad0024ab8
Mark Riffey <mriffey@rescuemarketing.com>
2020-10-05 00:09:18 -0600

    fixed typo

--- wpclean.py
+++ wpclean.py
@@ -25,7 +25,7 @@ txttoclean = txttoclean + '<i>Mark Riffey is an investor and advisor to small bu
              'Mark\'s site</a>, contact him on <a class="text" href="https://linkedin.com/in/markriffey" ' + \
              'target="_blank" rel="noopener noreferrer">LinkedIn</a> or <a class="text" ' + \
              'href="http://www.twitter.com/MarkRiffey" target="_blank" rel="noopener noreferrer">Twitter</a>,' + \
-             'or email him at <a class="text" href="mailto:mriffey@flatheadbeacon.com">' + \
+             ' or email him at <a class="text" href="mailto:mriffey@flatheadbeacon.com">' + \
              'mriffey@flatheadbeacon.com</a>.</i>'
 
 pyperclip.copy(txttoclean)
